2631b0caf7dc097742b0ba0658a2c06a36b5cf94
Itunu <47750536+iolaitan@users.noreply.github.com>
2022-11-02 17:20:08 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#ensures we do not get a 404 error and #defines what we want to me displayed
+@app.route('/')
+
+def render_the_map():
+    return render_template('All States.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
